290f49173c0bea9f68c812bbaccc6f2d14bf7ab2
Gay Stephenson <gaystephenson@me.com>
2019-04-25 16:39:01 -0700

    add paddle1 and paddle2, add KEYs and movement with limits to board

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,8 +1,9 @@
 
 
-import{ SVG_NS } from "../settings";
+import{ SVG_NS, KEYS } from "../settings";
 
 import Board from './Board';
+import Paddle from './Paddle';
 
 //console.log(SVG_NS);
 
@@ -17,11 +18,40 @@ export default class Game {
 
     this.board = new Board(this.width, this.height);
 
-		// Other code goes here...
+    this.paddleWidth = 8;
+    this.paddleHeight = 56;
+    this.boardGap = 10;
+    
+    this.player1 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      (this.height - this.paddleHeight) / 2,
+      KEYS.a,
+      KEYS.z
+    );
+
+    this.player2 = new Paddle(
+        this.height,
+        this.paddleWidth,
+        this.paddleHeight,
+        (this.width - this.boardGap - this.paddleWidth),
+        (this.height - this.paddleHeight) / 2,
+        KEYS.up,
+        KEYS.down
+
+        
+
+    );
+    console.log('player1');
+    console.log('player2');
+
   }
+  	// End of constructor
 
   render() {
-    // More code goes here....
+    // ....
     this.gameElement.innerHTML = '';
     let svg = document.createElementNS(SVG_NS, "svg");
     svg.setAttributeNS(null, "width", this.width);
@@ -30,6 +60,8 @@ export default class Game {
     this.gameElement.appendChild(svg);
     
     this.board.render(svg);
+    this.player1.render(svg);
+    this.player2.render(svg);
 
     
   }
